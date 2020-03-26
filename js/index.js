6b51c5744242cbe0048283426e98ce88ec5d84e3
genaromartinez <genaro.mtz1998@gmail.com>
2020-03-26 13:49:44 -0600

    remove text in input after submit

--- js/index.js
+++ js/index.js
@@ -1,22 +1,23 @@
 window.onload = function () {
-  let itemsList = [""];
 
   var textInput = document.getElementById("inputItem");
 
   document.getElementById('addBtn').addEventListener('click', function (e) {
     const name = textInput.value;
-    itemsList.push(name);
     const labelId = 'name' + (new Date()).getTime();
     var html = `
     <li class="shoppingItem">
-      <span id="${labelId}" >${textInput.value}</span>
+      <span id="${labelId}" >${name}</span>
+      <div class="btnContainer">
       <button id="btnCheck">check</button>
       <button id="btnDelete">delete</button>
+      </div>
     </li>`;
 
     insertHTML('ul', html, 'afterend');
-    addCheckEvent('btnCheck', labelId)
-    addDeleteEvent('btnDelete', labelId)
+    addCheckEvent('btnCheck', labelId);
+    addDeleteEvent('btnDelete', labelId);
+    textInput.value = "";
   })
 
   const insertHTML = (tag = 'ul', html, position = 'afterend', index = 0) => {
@@ -25,13 +26,14 @@ window.onload = function () {
   }
 
   const addCheckEvent = (id, target, event = 'click') => {
-    document.getElementById(id).addEventListener(event, function() {
+    document.getElementById(id).addEventListener(event, function () {
       document.getElementById(target).style.textDecoration = 'line-through'
     })
-    // $('.list .checkBtn').on('click', handleCheckButton(event));
   }
-      const addDeleteEvent = (id, target, event = 'click') => {
-        document.getElementById(id).addEventListener(event, function() {
-          document.getElementById(target).parentNode.parentNode.removeChild(document.getElementById(target).parentNode)
-        })
-}}
+
+  const addDeleteEvent = (id, target, event = 'click') => {
+    document.getElementById(id).addEventListener(event, function () {
+      document.getElementById(target).parentNode.parentNode.removeChild(document.getElementById(target).parentNode)
+    })
+  }
+}
