80f6f30bd21288e815d51a4807fdec3420e9128e
genaromartinez <genaro.mtz1998@gmail.com>
2020-03-26 00:23:38 -0600

    Add millisecond ids for unique assignment

--- js/index.js
+++ js/index.js
@@ -1,33 +1,37 @@
 window.onload = function () {
-  let itemsList = [];
+  let itemsList = [""];
 
   var textInput = document.getElementById("inputItem");
 
-  document.getElementById('store').addEventListener('click', function (e) {
-    itemsList.push(textInput.value);
-    var btnId = 'btnCheck';
-    var labelId = 'labelId'
-  
-    itemsList.forEach((item, index) => {
-      var html = `
-      <li>
-        <span id="${labelId}" >${item}</span>
-        <button id="${btnId}">check</button>
-        <button>delete</button>
-      </li>
-      `;
-      insertHTML('ul', html, 'afterend', index);
-    })
-    addEvent(btnId, labelId)
+  document.getElementById('addBtn').addEventListener('click', function (e) {
+    const name = textInput.value;
+    itemsList.push(name);
+    const labelId = 'name' + (new Date()).getTime();
+    var html = `
+    <li class="shoppingItem">
+      <span id="${labelId}" >${textInput.value}</span>
+      <button id="btnCheck">check</button>
+      <button id="btnDelete">delete</button>
+    </li>`;
+
+    insertHTML('ul', html, 'afterend');
+    addCheckEvent('btnCheck', labelId)
+    addDeleteEvent('btnDelete', labelId)
   })
 
   const insertHTML = (tag = 'ul', html, position = 'afterend', index = 0) => {
+    console.log(index)
     document.getElementsByTagName(tag)[index].insertAdjacentHTML(position, html);
   }
 
-  const addEvent = (id, target, event = 'click') => {
+  const addCheckEvent = (id, target, event = 'click') => {
     document.getElementById(id).addEventListener(event, function() {
       document.getElementById(target).style.textDecoration = 'line-through'
     })
     // $('.list .checkBtn').on('click', handleCheckButton(event));
+  }
+      const addDeleteEvent = (id, target, event = 'click') => {
+        document.getElementById(id).addEventListener(event, function() {
+          document.getElementById(target).parentNode.parentNode.removeChild(document.getElementById(target).parentNode)
+        })
 }}
